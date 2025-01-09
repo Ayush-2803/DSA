f21b9a7d11c028bd41f9b75e8a3f1b8ddfbe0e61
Ayush-2803 <ayush.github@gmail.com>
2025-01-09 12:48:51 +0530

    made a program that reads your name

--- practical1.c
+++ practical1.c
@@ -1,7 +1,21 @@
-#include<stdio.h>
-int main(){
-    char str[100];
+#include <stdio.h>
+
+int main() {
+    int length;       // Variable to store the number of words
+    printf("How many words are there in your name: ");
+    scanf("%d", &length);
+
+    char a[length][20];  // 2D array to store multiple words, assuming each word is up to 19 characters long
+
     printf("Enter your name: ");
-    scanf("%s",&str);
-    printf("My name is %s!",str);
+    for (int i = 0; i < length; i++) {
+        scanf("%19s", a[i]);  // Read each word into the 2D array
+    }
+
+    printf("Your name is: ");
+    for (int i = 0; i < length; i++) {
+        printf("%s ", a[i]);  // Print each word separated by a space
+    }
+
+    return 0;
 }
